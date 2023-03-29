8cb3dcae196af0bc6f2b70ab3efb568d2a35ab01
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-03-29 08:55:54 +0200

    Add files via upload

---
+++ WebPreglednik/WebPreglednik/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebPreglednik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Preglednik
+        {
+            string url;
+
+            public string Url { get => url; set => url = value; }
+        }
+        List<Preglednik> lista = new List<Preglednik>();
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //tab
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //bookmark
+            try
+            {
+                Preglednik bm = new Preglednik();
+                lista.Add(bm);
+
+
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
